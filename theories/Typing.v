1498d87cbb9ed652335f8a823c0b0593ad86e990
Théo Winterhalter <theo.winterhalter@gmail.com>
2023-12-15 10:47:31 +0100

    Draft typing

--- theories/Typing.v
+++ theories/Typing.v
@@ -4,4 +4,49 @@ From GhostTT Require Import BasicAST ContextDecl CastRemoval TermMode.
 
 Set Default Goal Selector "!".
 
-(* TODO Universe composition *)
+Reserved Notation "Γ ⊢ t : A"
+  (at level 80, t, A at next level, format "Γ  ⊢  t  :  A").
+
+Reserved Notation "Γ ⊢ u ≡ v"
+  (at level 80, u, v at next level, format "Γ  ⊢  u  ≡  v").
+
+Inductive typing (Γ : context) : term → term → Prop :=
+
+| type_var :
+    ∀ x m A,
+      nth_error Γ x = Some (m, A) →
+      Γ ⊢ var x : A
+
+where "Γ ⊢ t : A" := (typing Γ t A)
+
+with conversion (Γ : context) : term → term → Prop :=
+
+(* Computation rules *)
+
+(* Congruence rules *)
+
+| cong_Pi :
+    ∀ m mx A A' B B',
+      Γ ⊢ A ≡ A' →
+      Γ ,, (mx, A) ⊢ B ≡ B' →
+      Γ ⊢ Pi m mx A B ≡ Pi m mx A' B'
+
+
+(* Structural rules *)
+
+| conv_refl :
+    ∀ u,
+      Γ ⊢ u ≡ u
+
+| conv_sym :
+    ∀ u v,
+      Γ ⊢ u ≡ v →
+      Γ ⊢ v ≡ u
+
+| conv_trans :
+    ∀ u v w,
+      Γ ⊢ u ≡ v →
+      Γ ⊢ v ≡ w →
+      Γ ⊢ u ≡ w
+
+where "Γ ⊢ u ≡ v" := (conversion Γ u v).
