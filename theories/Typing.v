0e105c9fc9f5376e47e8aabd7bf912d03598c55f
Théo Winterhalter <theo.winterhalter@gmail.com>
2023-12-15 22:39:33 +0100

    Conversion rules

--- theories/Typing.v
+++ theories/Typing.v
@@ -1,6 +1,9 @@
 From Coq Require Import Utf8 List.
-From GhostTT.autosubst Require Import AST.
+From GhostTT.autosubst Require Import AST unscoped.
 From GhostTT Require Import BasicAST ContextDecl CastRemoval TermMode.
+
+(* TODO The substitution notation is broken, make new better ones? *)
+(* Import SubstNotations UnscopedNotations. *)
 
 Set Default Goal Selector "!".
 
@@ -23,6 +26,30 @@ with conversion (Γ : context) : term → term → Prop :=
 
 (* Computation rules *)
 
+| conv_irr :
+    ∀ p q,
+      md Γ p = mProp →
+      md Γ q = mProp →
+      Γ ⊢ p ≡ q
+
+| conv_beta :
+    ∀ mx A t u,
+      md Γ u = mx →
+      (* Γ ⊢ app (lam mx A t) u ≡ t [ u .. ] *)
+      Γ ⊢ app (lam mx A t) u ≡ subst1 (scons u ids) t
+
+| reveal_erase :
+    ∀ t P p,
+      In (md Γ p) [ mProp ; mGhost ] →
+      In (md Γ t) [ mType ; mKind ] →
+      Γ ⊢ reveal (erase t) P p ≡ app p t
+
+| revealP_erase :
+    ∀ t P p,
+      md Γ p = mKind →
+      md Γ t = mType →
+      Γ ⊢ revealP (erase t) p ≡ app p t
+
 (* Congruence rules *)
 
 | cong_Pi :
@@ -31,6 +58,59 @@ with conversion (Γ : context) : term → term → Prop :=
       Γ ,, (mx, A) ⊢ B ≡ B' →
       Γ ⊢ Pi m mx A B ≡ Pi m mx A' B'
 
+| cong_lam :
+    ∀ mx A A' t t',
+      Γ ⊢ A ≡ A' →
+      Γ ,, (mx, A) ⊢ t ≡ t' →
+      Γ ⊢ lam mx A t ≡ lam mx A' t'
+
+| cong_app :
+    ∀ u u' v v',
+      Γ ⊢ u ≡ u' →
+      Γ ⊢ v ≡ v' →
+      Γ ⊢ app u v ≡ app u' v'
+
+| cong_Erased :
+    ∀ A A',
+      Γ ⊢ A ≡ A' →
+      Γ ⊢ Erased A ≡ Erased A'
+
+| cong_erase :
+    ∀ u u',
+      Γ ⊢ u ≡ u' →
+      Γ ⊢ erase u ≡ erase u'
+
+| cong_reveal :
+    ∀ t t' P P' p p',
+      Γ ⊢ t ≡ t' →
+      Γ ⊢ P ≡ P' →
+      Γ ⊢ p ≡ p' →
+      Γ ⊢ reveal t P p ≡ reveal t' P' p'
+
+| cong_revealP :
+    ∀ t t' p p',
+      Γ ⊢ t ≡ t' →
+      Γ ⊢ p ≡ p' →
+      Γ ⊢ revealP t p ≡ revealP t' p'
+
+| cong_gheq :
+    ∀ A A' u u' v v',
+      Γ ⊢ A ≡ A' →
+      Γ ⊢ u ≡ u' →
+      Γ ⊢ v ≡ v' →
+      Γ ⊢ gheq A u v ≡ gheq A' u' v'
+
+(* No need for it thanks to proof irrelevance *)
+(* | cong_ghrefl :
+    ∀ A A' u u',
+      Γ ⊢ A ≡ A' →
+      Γ ⊢ u ≡ u' →
+      Γ ⊢ ghrefl A u ≡ ghrefl A' u' *)
+
+| cong_bot_elim :
+    ∀ m A A' p p',
+      Γ ⊢ A ≡ A' →
+      Γ ⊢ bot_elim m A p ≡ bot_elim m A' p'
 
 (* Structural rules *)
 
