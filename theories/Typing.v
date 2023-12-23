a2045231f5813ccff1215ebdf229dfaf0e18f667
Théo Winterhalter <theo.winterhalter@gmail.com>
2023-12-23 16:51:35 +0100

    Conversion is no longer mutual with typing

--- theories/Typing.v
+++ theories/Typing.v
@@ -19,6 +19,126 @@ Reserved Notation "Γ ⊢ t : A"
 Reserved Notation "Γ ⊢ u ≡ v"
   (at level 80, u, v at next level, format "Γ  ⊢  u  ≡  v").
 
+Inductive conversion (Γ : context) : term → term → Prop :=
+
+(* Computation rules *)
+
+| conv_irr :
+    ∀ p q,
+      cscoping Γ p mProp →
+      cscoping Γ q mProp →
+      Γ ⊢ p ≡ q
+
+| conv_beta :
+    ∀ m mx A t u,
+      cscoping Γ A mKind →
+      scoping (mx :: sc Γ) t m →
+      cscoping Γ u mx →
+      Γ ⊢ app (lam mx A t) u ≡ t <[ u .. ]
+
+| reveal_erase :
+    ∀ mp t P p,
+      cscoping Γ t mType →
+      cscoping Γ P mKind →
+      cscoping Γ p mp →
+      In mp [ mProp ; mGhost ] →
+      Γ ⊢ reveal (erase t) P p ≡ app p t
+
+| revealP_erase :
+    ∀ t p,
+      cscoping Γ p mKind →
+      cscoping Γ t mType →
+      Γ ⊢ revealP (erase t) p ≡ app p t
+
+(* Congruence rules *)
+
+(* A rule to quotient away all levels of Prop, making it impredicative *)
+| cong_Prop :
+    ∀ i j,
+      Γ ⊢ Sort mProp i ≡ Sort mProp j
+
+| cong_Pi :
+    ∀ m mx A A' B B',
+      Γ ⊢ A ≡ A' →
+      Γ ,, (mx, A) ⊢ B ≡ B' →
+      Γ ⊢ Pi m mx A B ≡ Pi m mx A' B'
+
+| cong_lam :
+    ∀ mx A A' t t',
+      Γ ⊢ A ≡ A' →
+      Γ ,, (mx, A) ⊢ t ≡ t' →
+      Γ ⊢ lam mx A t ≡ lam mx A' t'
+
+| cong_app :
+    ∀ u u' v v',
+      Γ ⊢ u ≡ u' →
+      Γ ⊢ v ≡ v' →
+      Γ ⊢ app u v ≡ app u' v'
+
+| cong_Erased :
+    ∀ A A',
+      Γ ⊢ A ≡ A' →
+      Γ ⊢ Erased A ≡ Erased A'
+
+| cong_erase :
+    ∀ u u',
+      Γ ⊢ u ≡ u' →
+      Γ ⊢ erase u ≡ erase u'
+
+| cong_reveal :
+    ∀ t t' P P' p p',
+      Γ ⊢ t ≡ t' →
+      Γ ⊢ P ≡ P' →
+      Γ ⊢ p ≡ p' →
+      Γ ⊢ reveal t P p ≡ reveal t' P' p'
+
+| cong_revealP :
+    ∀ t t' p p',
+      Γ ⊢ t ≡ t' →
+      Γ ⊢ p ≡ p' →
+      Γ ⊢ revealP t p ≡ revealP t' p'
+
+| cong_gheq :
+    ∀ A A' u u' v v',
+      Γ ⊢ A ≡ A' →
+      Γ ⊢ u ≡ u' →
+      Γ ⊢ v ≡ v' →
+      Γ ⊢ gheq A u v ≡ gheq A' u' v'
+
+(* No need for it thanks to proof irrelevance *)
+(* | cong_ghrefl :
+    ∀ A A' u u',
+      Γ ⊢ A ≡ A' →
+      Γ ⊢ u ≡ u' →
+      Γ ⊢ ghrefl A u ≡ ghrefl A' u' *)
+
+(* Maybe not needed? *)
+| cong_bot_elim :
+    ∀ m A A' p p',
+      Γ ⊢ A ≡ A' →
+      (* Needed because syntactically we don't know p and p' are irrelevant *)
+      Γ ⊢ p ≡ p' →
+      Γ ⊢ bot_elim m A p ≡ bot_elim m A' p'
+
+(* Structural rules *)
+
+| conv_refl :
+    ∀ u,
+      Γ ⊢ u ≡ u
+
+| conv_sym :
+    ∀ u v,
+      Γ ⊢ u ≡ v →
+      Γ ⊢ v ≡ u
+
+| conv_trans :
+    ∀ u v w,
+      Γ ⊢ u ≡ v →
+      Γ ⊢ v ≡ w →
+      Γ ⊢ u ≡ w
+
+where "Γ ⊢ u ≡ v" := (conversion Γ u v).
+
 Inductive typing (Γ : context) : term → term → Prop :=
 
 | type_var :
@@ -145,124 +265,4 @@ Inductive typing (Γ : context) : term → term → Prop :=
       Γ ⊢ B : Sort m i →
       Γ ⊢ t : B
 
-where "Γ ⊢ t : A" := (typing Γ t A)
-
-with conversion (Γ : context) : term → term → Prop :=
-
-(* Computation rules *)
-
-| conv_irr :
-    ∀ p q,
-      cscoping Γ p mProp →
-      cscoping Γ q mProp →
-      Γ ⊢ p ≡ q
-
-| conv_beta :
-    ∀ m mx A t u,
-      cscoping Γ A mKind →
-      scoping (mx :: sc Γ) t m →
-      cscoping Γ u mx →
-      Γ ⊢ app (lam mx A t) u ≡ t <[ u .. ]
-
-| reveal_erase :
-    ∀ mp t P p,
-      cscoping Γ t mType →
-      cscoping Γ P mKind →
-      cscoping Γ p mp →
-      In mp [ mProp ; mGhost ] →
-      Γ ⊢ reveal (erase t) P p ≡ app p t
-
-| revealP_erase :
-    ∀ t p,
-      cscoping Γ p mKind →
-      cscoping Γ t mType →
-      Γ ⊢ revealP (erase t) p ≡ app p t
-
-(* Congruence rules *)
-
-(* A rule to quotient away all levels of Prop, making it impredicative *)
-| cong_Prop :
-    ∀ i j,
-      Γ ⊢ Sort mProp i ≡ Sort mProp j
-
-| cong_Pi :
-    ∀ m mx A A' B B',
-      Γ ⊢ A ≡ A' →
-      Γ ,, (mx, A) ⊢ B ≡ B' →
-      Γ ⊢ Pi m mx A B ≡ Pi m mx A' B'
-
-| cong_lam :
-    ∀ mx A A' t t',
-      Γ ⊢ A ≡ A' →
-      Γ ,, (mx, A) ⊢ t ≡ t' →
-      Γ ⊢ lam mx A t ≡ lam mx A' t'
-
-| cong_app :
-    ∀ u u' v v',
-      Γ ⊢ u ≡ u' →
-      Γ ⊢ v ≡ v' →
-      Γ ⊢ app u v ≡ app u' v'
-
-| cong_Erased :
-    ∀ A A',
-      Γ ⊢ A ≡ A' →
-      Γ ⊢ Erased A ≡ Erased A'
-
-| cong_erase :
-    ∀ u u',
-      Γ ⊢ u ≡ u' →
-      Γ ⊢ erase u ≡ erase u'
-
-| cong_reveal :
-    ∀ t t' P P' p p',
-      Γ ⊢ t ≡ t' →
-      Γ ⊢ P ≡ P' →
-      Γ ⊢ p ≡ p' →
-      Γ ⊢ reveal t P p ≡ reveal t' P' p'
-
-| cong_revealP :
-    ∀ t t' p p',
-      Γ ⊢ t ≡ t' →
-      Γ ⊢ p ≡ p' →
-      Γ ⊢ revealP t p ≡ revealP t' p'
-
-| cong_gheq :
-    ∀ A A' u u' v v',
-      Γ ⊢ A ≡ A' →
-      Γ ⊢ u ≡ u' →
-      Γ ⊢ v ≡ v' →
-      Γ ⊢ gheq A u v ≡ gheq A' u' v'
-
-(* No need for it thanks to proof irrelevance *)
-(* | cong_ghrefl :
-    ∀ A A' u u',
-      Γ ⊢ A ≡ A' →
-      Γ ⊢ u ≡ u' →
-      Γ ⊢ ghrefl A u ≡ ghrefl A' u' *)
-
-(* Maybe not needed? *)
-| cong_bot_elim :
-    ∀ m A A' p p',
-      Γ ⊢ A ≡ A' →
-      (* Needed because syntactically we don't know p and p' are irrelevant *)
-      Γ ⊢ p ≡ p' →
-      Γ ⊢ bot_elim m A p ≡ bot_elim m A' p'
-
-(* Structural rules *)
-
-| conv_refl :
-    ∀ u,
-      Γ ⊢ u ≡ u
-
-| conv_sym :
-    ∀ u v,
-      Γ ⊢ u ≡ v →
-      Γ ⊢ v ≡ u
-
-| conv_trans :
-    ∀ u v w,
-      Γ ⊢ u ≡ v →
-      Γ ⊢ v ≡ w →
-      Γ ⊢ u ≡ w
-
-where "Γ ⊢ u ≡ v" := (conversion Γ u v).
+where "Γ ⊢ t : A" := (typing Γ t A).
