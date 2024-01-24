fee64b0cb0510216316b9a55158dae150d0db548
Théo Winterhalter <theo.winterhalter@gmail.com>
2024-01-24 09:29:17 +0100

    Prove revival_ng

--- theories/Revival.v
+++ theories/Revival.v
@@ -24,7 +24,7 @@ Reserved Notation "⟦ G | u '⟧v'" (at level 9, G, u at next level).
 Equations revive_term (Γ : scope) (t : term) : cterm := {
   ⟦ Γ | var x ⟧v := if ghv Γ x then cvar x else cDummy ;
   ⟦ Γ | lam mx A B t ⟧v :=
-    if isGhost (md Γ t)
+    if isGhost (md (mx :: Γ) t)
     then
       if isProp mx
       then clam cType ⟦ Γ | A ⟧τ ⟦ mx :: Γ | t ⟧v
@@ -50,3 +50,43 @@ Equations revive_term (Γ : scope) (t : term) : cterm := {
   ⟦ _ | _ ⟧v := cDummy
 }
 where "⟦ G | u '⟧v'" := (revive_term G u).
+
+Reserved Notation "⟦ G '⟧v'" (at level 9, G at next level).
+
+Equations revive_ctx (Γ : context) : ccontext := {
+  ⟦ [] ⟧v := [] ;
+  ⟦ Γ ,, (mx, A) ⟧v :=
+    if isProp mx
+    then None :: ⟦ Γ ⟧v
+    else Some (cType, ⟦ sc Γ | A ⟧τ) :: ⟦ Γ ⟧v
+}
+where "⟦ G '⟧v'" := (revive_ctx G).
+
+Definition revive_sc (Γ : scope) : cscope :=
+  map (λ m, if isProp m then None else Some cType) Γ.
+
+(** Revival of non-ghost terms is cDummy **)
+
+Lemma revival_ng :
+  ∀ Γ t,
+    isGhost (md Γ t) = false →
+    ⟦ Γ | t ⟧v = cDummy.
+Proof.
+  intros Γ t hm.
+  induction t in Γ, hm |- *.
+  all: try reflexivity.
+  all: try discriminate hm.
+  - cbn in *. unfold ghv.
+    destruct (nth_error _ _) eqn:e. 2: reflexivity.
+    erewrite nth_error_nth in hm. 2: eassumption.
+    rewrite hm. reflexivity.
+  - cbn - [mode_inb] in *.
+    rewrite hm. reflexivity.
+  - cbn - [mode_inb] in *.
+    rewrite hm. reflexivity.
+  - cbn - [mode_inb] in *.
+    destruct_if e. 2: reflexivity.
+    destruct (md Γ _) eqn:e'. all: discriminate.
+  - cbn - [mode_inb] in *. eauto.
+  - cbn - [mode_inb] in *. rewrite hm. reflexivity.
+Qed.
