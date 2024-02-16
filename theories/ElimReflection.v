63476b9523ad6eb19ecd8e37d56366b09c5d79fb
Théo Winterhalter <theo.winterhalter@gmail.com>
2024-02-16 10:59:32 +0100

    App case

--- theories/ElimReflection.v
+++ theories/ElimReflection.v
@@ -14,6 +14,20 @@ Import CombineNotations.
 Set Default Goal Selector "!".
 Set Equations Transparent.
 
+Lemma tr_pi :
+  ∀ i j m mx A B Γ' A' B',
+    wf Γ' →
+    Γ' ⊨ A' : Sort mx i ∈ ⟦ A : Sort mx i ⟧x →
+    (Γ',, (mx,A')) ⊨ B' : Sort m j ∈ ⟦ B : Sort m j ⟧x →
+    Γ' ⊨ (Pi i j m mx A' B') : (Sort m (umax mx m i j)) ∈
+    ⟦ (Pi i j m mx A B) : (Sort m (umax mx m i j)) ⟧x.
+Proof.
+  intros i j m mx A B Γ' A' B' hΓ hA hB.
+  unfold tr_ty in *. intuition subst.
+  - eapply type_pi. all: eassumption.
+  - cbn. intuition reflexivity.
+Qed.
+
 Theorem elim_reflection :
   ∀ Γ t A Γ',
     Γ ⊢ˣ t : A →
@@ -37,10 +51,9 @@ Proof.
     eapply tr_cons in hA as hext. 2: eassumption.
     specialize IHh2 with (1 := hext). destruct IHh2 as [B' [s'' hB]].
     eapply tr_sort' in hB. 2: apply hext.
-    unfold tr_ctx, tr_ty in *. intuition subst.
-    eexists (Pi i j m mx A' B'), _. split.
-    + eapply type_pi. all: eassumption.
-    + cbn. intuition reflexivity.
+    destruct hctx.
+    eexists (Pi i j m mx A' B'), _.
+    eapply tr_pi. all: eassumption.
   - specialize IHh1 with (1 := hctx). destruct IHh1 as [A' [s' hA]].
     eapply tr_sort' in hA. 2: apply hctx.
     eapply tr_cons in hA as hext. 2: eassumption.
@@ -52,4 +65,19 @@ Proof.
     eexists (lam mx A' B' t'), _. split.
     + eapply type_lam. all: eauto.
     + cbn. intuition reflexivity.
+  - specialize IHh3 with (1 := hctx). destruct IHh3 as [A' [s' hA]].
+    eapply tr_sort' in hA. 2: apply hctx.
+    eapply tr_cons in hA as hext. 2: eassumption.
+    specialize IHh4 with (1 := hext). destruct IHh4 as [B' [s'' hB]].
+    eapply tr_sort' in hB. 2: apply hext.
+    eapply tr_pi in hB as hPi. 2: apply hctx. 2: eassumption.
+    specialize IHh1 with (1 := hctx). destruct IHh1 as [t' [P' ht]].
+    eapply tr_choice in ht. 2-4: eassumption.
+    specialize IHh2 with (1 := hctx). destruct IHh2 as [u' [A'' hu]].
+    eapply tr_choice in hu. 2-4: eassumption.
+    unfold tr_ctx, tr_ty in *. intuition subst.
+    eexists (app t' u'), _. split.
+    + eapply type_app. all: eassumption.
+    + cbn. intuition eauto.
+      rewrite castrm_subst. ssimpl. reflexivity.
 Abort.
