1ddd961e11b8d820b7e986670e0e483937ce04ec
Théo Winterhalter <theo.winterhalter@gmail.com>
2024-05-28 14:47:54 +0200

    Complete discr term!

--- theories/Examples.v
+++ theories/Examples.v
@@ -337,13 +337,19 @@ Proof.
   - apply rtyping_triv.
 Qed.
 
-Axiom dumdum : term.
-
 Definition erased_nat_discr :=
   lam mGhost (Erased tnat) (
-    lam mProp (gheq (Erased tnat) (hide tzero) (gS (var 0))) (
-      fromRev dumdum dumdum (ghcast (Erased tnat) (hide tzero) (gS (var 1)) (var 0) erased_nat_discrP (toRev tzero discr_nat star))
-    )
+    reveal (var 0)
+      (lam mGhost (Erased tnat) (gheq (Erased tnat) (hide tzero) (gS (var 0)) ⇒[ 0 | 0 / mProp | mProp ] bot))
+      (lam mType tnat (
+        lam mProp (gheq (Erased tnat) (hide tzero) (hide (tsucc (var 0)))) (
+          fromRev (tsucc (var 1)) discr_nat (
+            ghcast (Erased tnat) (hide tzero) (hide (tsucc (var 1))) (var 0) erased_nat_discrP (
+              toRev tzero discr_nat star
+            )
+          )
+        )
+      ))
   ).
 
 Lemma type_erased_nat_discr :
@@ -355,6 +361,22 @@ Lemma type_erased_nat_discr :
 Proof.
   assert (hw : wf [ (mGhost, Erased tnat) ]).
   { eapply wf_cons. 1: constructor. gtype. }
+  assert (hw2 : wf [ (mGhost, Erased tnat) ; (mGhost, Erased tnat) ]).
+  { eapply wf_cons. 1: auto. gtype. }
+  assert (hw3 : wf [ (mType, tnat) ; (mGhost, Erased tnat) ]).
+  { eapply wf_cons. 1: auto. gtype. }
+  assert (hw4 : wf [ (mGhost, Erased tnat) ; (mType, tnat) ; (mGhost, Erased tnat) ]).
+  { eapply wf_cons. 1: auto. gtype. }
+  assert (hge : [ (mType, tnat); (mGhost, Erased tnat) ] ⊢ gheq (Erased tnat) (hide tzero) (hide (tsucc (var 0))) : Sort mProp 0).
+  { eapply type_gheq. 1: auto. 1,2: gtype.
+    eapply type_hide. 1: auto. 1: gtype.
+    eapply type_succ. 1:{ gscope. reflexivity. }
+    eapply meta_conv.
+    - gtype. reflexivity.
+    - reflexivity.
+  }
+  assert (hw5 : wf [ (mProp, gheq (Erased tnat) (hide tzero) (hide (tsucc (var 0)))) ; (mType, tnat) ; (mGhost, Erased tnat) ]).
+  { eapply wf_cons. all: eauto. }
   cbn. eapply type_lam. 1: constructor. 1: gtype.
   - eapply type_pi. 1: auto. 2: gtype.
     eapply type_gheq. 1: auto. all: gtype.
@@ -362,40 +384,123 @@ Proof.
     eapply meta_conv.
     + gtype. reflexivity.
     + reflexivity.
-  - assert (hg : [(mGhost, Erased tnat)] ⊢ gheq (Erased tnat) (hide tzero) (gS (var 0)) : Sort mProp 0).
+  - assert (hg : [(mGhost, Erased tnat) ; (mGhost, Erased tnat)] ⊢ gheq (Erased tnat) (hide tzero) (gS (var 0)) : Sort mProp 0).
     { eapply type_gheq. 1: auto. 1,2: gtype.
       eapply type_gS. 1: auto.
       eapply meta_conv.
       - gtype. reflexivity.
       - reflexivity.
     }
-    eapply type_lam. 1: auto. 2: gtype. 1: auto.
-    assert (hw2 : wf [ (mProp, gheq (Erased tnat) (hide tzero) (gS (var 0))) ; (mGhost, Erased tnat) ]).
-    { eapply wf_cons. all: eauto. }
+    assert (hw6 : wf [(mType, tnat); (mGhost, Erased tnat); (mType, tnat); (mGhost, Erased tnat)]).
+    { eapply wf_cons. 1: auto. gtype. }
     eapply type_conv_alt. 1: auto.
-    + eapply type_fromRev. 1: auto.
-      3:{
+    + eapply type_reveal. 1: auto.
+      2:{
+        eapply meta_conv.
+        - gtype. reflexivity.
+        - reflexivity.
+      }
+      2:{
+        eapply type_lam. all: cbn. 1: auto. 1,2: gtype.
+        eapply type_pi. 1,2: auto.
+        gtype.
+      }
+      1: cbn ; auto.
+      cbn. eapply type_lam. 1: auto. 1: gtype.
+      * {
+        eapply meta_conv.
+        - eapply type_app. 1: auto.
+          + eapply type_lam. 1: auto. 1: gtype.
+            2:{
+              eapply type_pi. 1: auto.
+              - eapply type_gheq. 1: auto. 1,2: gtype.
+                eapply type_gS. 1: auto.
+                eapply meta_conv.
+                + gtype. reflexivity.
+                + reflexivity.
+              - gtype.
+            }
+            cbn. gtype.
+          + eapply type_hide. 1: auto. 1: gtype.
+            eapply meta_conv.
+            * gtype. reflexivity.
+            * reflexivity.
+        - reflexivity.
+      }
+      * {
         eapply type_conv_alt. 1: auto.
-        - eapply type_ghcast. 1: auto.
+        - eapply type_lam. 1,2: eauto.
           2:{
-            eapply meta_conv.
-            - gtype. reflexivity.
-            - cbn. reflexivity.
-          }
-          2:{
-            eapply meta_conv.
-            - eapply type_erased_nat_discrP_gen.
-            - cbn. f_equal.
-          }
-          1: discriminate.
-          eapply type_conv_alt. 1: auto.
-          + eapply type_toRev. 1: auto.
-            * eapply type_zero.
-            * eapply type_discr_nat_gen.
-            * {
-              eapply type_conv. 1: auto. 1:{ gscope. reflexivity. }
-              - eapply type_star_gen.
-              - apply conv_sym. Transparent discr_nat discr_bool iszero. cbn.
+            eapply type_fromRev. 1: auto.
+            1:{
+              eapply type_succ. 1:{ gscope. reflexivity. }
+              eapply meta_conv.
+              - gtype. reflexivity.
+              - reflexivity.
+            }
+            1: eapply type_discr_nat_gen.
+            eapply type_conv_alt. 1: auto.
+            - eapply type_ghcast. 1: auto.
+              2:{
+                eapply meta_conv.
+                - gtype. reflexivity.
+                - reflexivity.
+              }
+              2:{
+                eapply meta_conv.
+                - eapply type_erased_nat_discrP_gen.
+                - cbn. f_equal.
+              }
+              1: discriminate.
+              eapply type_conv_alt. 1: auto.
+              + eapply type_toRev. 1: auto.
+                * eapply type_zero.
+                * eapply type_discr_nat_gen.
+                * {
+                  eapply type_conv. 1: auto. 1:{ gscope. reflexivity. }
+                  - eapply type_star_gen.
+                  - apply conv_sym. Transparent discr_nat discr_bool iszero.
+                    cbn. eapply conv_trans.
+                    1:{
+                      eapply conv_beta.
+                      - gscope.
+                      - gscope. all: try reflexivity. discriminate.
+                      - gscope.
+                    }
+                    cbn. eapply conv_trans.
+                    1:{
+                      eapply conv_beta.
+                      - gscope.
+                      - gscope. reflexivity.
+                      - gscope.
+                        + discriminate.
+                        + reflexivity.
+                    }
+                    cbn. eapply conv_trans.
+                    1:{
+                      econstructor. 2-4: apply conv_refl.
+                      eapply conv_beta.
+                      - gscope.
+                      - gscope. 2: reflexivity. discriminate.
+                      - gscope.
+                    }
+                    cbn. eapply conv_trans.
+                    1:{
+                      econstructor. 2-4: apply conv_refl.
+                      eapply conv_nat_elim_zero.
+                      - discriminate.
+                      - gscope.
+                      - gscope.
+                      - gscope.
+                    }
+                    apply conv_if_true.
+                    gscope.
+                  - eapply meta_conv.
+                    + eapply type_app. 1: auto. 1: eapply type_discr_nat_gen.
+                      eapply type_zero.
+                    + reflexivity.
+                }
+              + apply conv_sym. Transparent erased_nat_discrP. cbn.
                 eapply conv_trans.
                 1:{
                   eapply conv_beta.
@@ -403,71 +508,169 @@ Proof.
                   - gscope. all: try reflexivity. discriminate.
                   - gscope.
                 }
-                cbn. eapply conv_trans.
-                1:{
-                  eapply conv_beta.
-                  - gscope.
-                  - gscope. reflexivity.
-                  - gscope.
-                    + discriminate.
-                    + reflexivity.
-                }
-                cbn. eapply conv_trans.
-                1:{
-                  econstructor. 2-4: apply conv_refl.
-                  eapply conv_beta.
-                  - gscope.
-                  - gscope.
-                    + discriminate.
-                    + reflexivity.
-                  - gscope.
-                }
-                cbn. eapply conv_trans.
-                1:{
-                  econstructor. 2-4: apply conv_refl.
-                  eapply conv_nat_elim_zero.
-                  - discriminate.
-                  - gscope.
-                  - gscope.
-                  - gscope.
-                }
-                econstructor. gscope.
-              - eapply meta_conv.
-                + eapply type_app. 1: auto.
-                  * eapply type_discr_nat_gen.
-                  * eapply type_zero.
-                + reflexivity.
+                cbn. apply conv_refl.
+              + eapply type_Reveal. 1: auto. 2: eapply type_discr_nat_gen.
+                eapply type_hide. 1: auto. all: gtype.
+              + eapply meta_conv.
+                * eapply type_app. 1: auto.
+                  1: eapply type_erased_nat_discrP_gen.
+                  gtype.
+                * reflexivity.
+            - cbn. eapply conv_trans.
+              1:{
+                eapply conv_beta. all: gscope. all: try reflexivity.
+                discriminate.
+              }
+              cbn. apply conv_refl.
+            - eapply meta_conv.
+              + eapply type_app. 1: auto. 1: eapply type_erased_nat_discrP_gen.
+                eapply type_hide. 1: auto. 1: gtype.
+                eapply type_succ. 1:{ gscope. reflexivity. }
+                eapply meta_conv.
+                * gtype. reflexivity.
+                * reflexivity.
+              + reflexivity.
+            - eapply type_Reveal. 1: auto. 2: eapply type_discr_nat_gen.
+              eapply type_hide. 1: auto. 1: gtype.
+              eapply type_succ. 1:{ gscope. reflexivity. }
+              eapply meta_conv.
+              + gtype. reflexivity.
+              + reflexivity.
+          }
+          eapply meta_conv.
+          + eapply type_app. 1: auto. 1: eapply type_discr_nat_gen.
+            eapply type_succ. 1:{ gscope. reflexivity. }
+            eapply meta_conv.
+            * gtype. reflexivity.
+            * reflexivity.
+          + reflexivity.
+        - apply conv_sym. cbn. eapply conv_trans.
+          1:{
+            eapply conv_beta. all: gscope. all: try reflexivity.
+            cbn. auto.
+          }
+          cbn. gconv.
+          3,4: unfold ueq ; auto.
+          + eapply conv_trans.
+            1:{
+              econstructor. all: gscope. all: try reflexivity.
+              cbn. auto.
             }
-          + apply conv_sym. Transparent erased_nat_discrP. cbn.
+            eapply conv_beta. all: gscope. all: reflexivity.
+          + apply conv_sym. eapply conv_trans.
+            1:{
+              eapply conv_beta. all: gscope. all: try reflexivity.
+              discriminate.
+            }
+            cbn. eapply conv_trans.
+            1:{
+              eapply conv_beta. all: gscope. all: try reflexivity.
+              discriminate.
+            }
+            cbn. eapply conv_trans.
+            1:{
+              econstructor. 2-4: apply conv_refl.
+              eapply conv_beta. all: gscope. all: try reflexivity.
+              discriminate.
+            }
+            cbn. eapply conv_trans.
+            1:{
+              econstructor. 2-4: apply conv_refl.
+              econstructor. all: gscope. all: try reflexivity.
+              discriminate.
+            }
             eapply conv_trans.
             1:{
-              eapply conv_beta.
-              - gscope.
-              - gscope. all: try reflexivity. discriminate.
-              - gscope.
+              econstructor. 2-4: apply conv_refl.
+              econstructor. 2: apply conv_refl.
+              econstructor. all: gscope. reflexivity.
             }
-            cbn. apply conv_refl.
-          + eapply type_Reveal. 1: auto.
-            * eapply type_hide. 1: auto. 2: eapply type_zero.
-              eapply type_nat.
-            * eapply type_discr_nat_gen.
-          + eapply meta_conv.
-            * eapply type_app. 1: auto. 1: eapply type_erased_nat_discrP_gen.
-              eapply type_hide. 1: auto. 2: eapply type_zero.
-              eapply type_nat.
+            cbn. eapply conv_trans.
+            1:{
+              econstructor. 2-4: apply conv_refl.
+              econstructor. all: gscope. 2: reflexivity.
+              discriminate.
+            }
+            cbn. econstructor. gscope.
+        - eapply type_pi. 1: auto. 1: auto.
+          eapply meta_conv.
+          + eapply type_app. 1: auto. 1: eapply type_discr_nat_gen.
+            eapply type_succ. 1:{ gscope. reflexivity. }
+            eapply meta_conv.
+            * gtype. reflexivity.
             * reflexivity.
-        - cbn. eapply conv_trans.
-          1:{
-            eapply conv_beta.
-            - gscope.
-            - gscope. all: try reflexivity. discriminate.
-            - gscope. all: try reflexivity. cbn. auto.
-          }
-          cbn. give_up. (* It's broken isn't it? *)
+          + reflexivity.
+        - eapply meta_conv.
+          + eapply type_app. 1: auto.
+            2:{
+              eapply type_hide. 1: auto.
+              2:{ gtype. reflexivity. }
+              cbn. gtype.
+            }
+            cbn. eapply type_lam. 1: auto. 1: gtype.
+            2:{
+              eapply type_pi. 1: auto. 2: gtype.
+              eapply type_gheq. 1: auto. 1,2: gtype.
+              eapply type_conv. 1: auto.
+              1:{ gscope. all: try reflexivity. cbn. auto. }
+              - eapply type_reveal. 1: auto.
+                2:{
+                  eapply meta_conv.
+                  - gtype. reflexivity.
+                  - reflexivity.
+                }
+                2:{
+                  eapply type_lam. all: cbn. 1: auto. 1-3: gtype.
+                }
+                1:{ cbn. auto. }
+                cbn. eapply type_lam. 1: auto. 1: gtype.
+                2:{
+                  eapply type_conv. 1: auto.
+                  1:{ gscope. reflexivity. }
+                  - eapply type_hide. 1: auto.
+                    2:{
+                      eapply type_succ. 1:{ gscope. reflexivity. }
+                      eapply meta_conv.
+                      - gtype. reflexivity.
+                      - reflexivity.
+                    }
+                    gtype.
+                  - cbn. apply conv_sym. gconv. reflexivity.
+                  - eapply meta_conv.
+                    + eapply type_app. 1: auto.
+                      * eapply type_lam. 1: auto. 1,3: gtype. gtype.
+                      * eapply type_hide. 1: auto. 1: gtype.
+                        eapply meta_conv. 1: gtype. all: reflexivity.
+                    + reflexivity.
+                }
+                eapply meta_conv.
+                + eapply type_app. 1: auto.
+                  * eapply type_lam. 1: auto. 1,3: gtype. gtype.
+                  * eapply type_hide. 1: auto. 1: gtype.
+                    eapply meta_conv. 1: gtype. all: reflexivity.
+                + reflexivity.
+              - cbn. gconv. reflexivity.
+              - gtype.
+            }
+            cbn. gtype.
+          + reflexivity.
       }
-      * admit.
-      * admit.
-    + admit.
-    + admit.
-    + admit.
-Abort.
+    + cbn. eapply conv_trans.
+      1:{
+        econstructor. all: gscope. all: try reflexivity.
+        cbn. auto.
+      }
+      cbn. apply conv_refl.
+    + eapply meta_conv.
+      * eapply type_app. 1: auto. 2:{ gtype. reflexivity. }
+        cbn. eapply type_lam. 1: auto. 1: gtype.
+        2:{
+          eapply type_pi. 1: auto. 1: auto. gtype.
+        }
+        cbn. gtype.
+      * reflexivity.
+    + eapply type_pi. 1: auto. 2: gtype.
+      eapply type_gheq. 1: auto. 1,2: gtype.
+      eapply type_gS. 1: auto.
+      eapply meta_conv. 1: gtype. all: reflexivity.
+Qed.
