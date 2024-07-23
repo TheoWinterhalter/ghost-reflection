4a84ad0b0225bf3ff6b1189c504fe9243376af8a
Théo Winterhalter <theo.winterhalter@gmail.com>
2024-07-23 15:25:00 +0200

    Trying something simpler

--- theories/SubstNotations.v
+++ theories/SubstNotations.v
@@ -35,50 +35,23 @@ Ltac ssimpl :=
   asimpl ;
   repeat unfold_funcomp.
 
-(** Autosubst NbE tactic that should be more efficient than asimpl/ssimpl
-
-  We reproduce what asimpl' does first:
-  - Make sure each term only has at most one substitution ore renaming applied
-    to it, resorting to composition instead.
-  - Apply renamings and substitutions to variables.
-  - Get rid of identity renamings and substitutions.
-  - Calls fsimpl to simplify expressions.
-
-**)
-
-About substSubst_cterm_pointwise.
-About substSubst_cterm.
-About substRen_cterm_pointwise.
-About substRen_cterm.
-About renSubst_cterm_pointwise.
-About renSubst_cterm.
-About renRen'_cterm_pointwise.
-About renRen_cterm.
-About varLRen'_cterm_pointwise.
-About varLRen'_cterm.
-About varL'_cterm_pointwise.
-About varL'_cterm.
-About rinstId'_cterm_pointwise.
-About rinstId'_cterm.
-About instId'_cterm_pointwise.
-About instId'_cterm.
-
-Print up_cterm_cterm.
-Print upRen_cterm_cterm.
-Print up_ren.
-
-Print funcomp.
+(** Autosubst reflective tactic that should be more efficient than asimpl **)
 
 Inductive quoted_ren :=
 | qren_atom (ρ : nat → nat)
-| qren_comp (r q : quoted_ren).
+| qren_comp (r q : quoted_ren)
+| qren_cons (n : nat) (q : quoted_ren)
+| qren_id
+| qren_shift.
 
 Inductive quoted_subst :=
 | qsubst_atom (σ : nat → cterm)
-| qsubst_comp (s t : quoted_subst).
+| qsubst_comp (s t : quoted_subst)
+| qsubst_cons (t : cterm) (s : quoted_subst)
+| qsubst_id.
 
 Inductive quoted_cterm :=
-| qmeta (t : cterm)
+| qatom (t : cterm)
 | qren (r : quoted_ren) (t : quoted_cterm)
 | qsubst (s : quoted_subst) (t : quoted_cterm).
 
@@ -86,150 +59,145 @@ Fixpoint unquote_ren q :=
   match q with
   | qren_atom ρ => ρ
   | qren_comp r q => funcomp (unquote_ren r) (unquote_ren q)
+  | qren_cons n q => scons n (unquote_ren q)
+  | qren_id => id
+  | qren_shift => shift
   end.
 
 Fixpoint unquote_subst q :=
   match q with
   | qsubst_atom σ => σ
   | qsubst_comp s t => funcomp (subst_cterm (unquote_subst s)) (unquote_subst t)
+  | qsubst_cons t s => scons t (unquote_subst s)
+  | qsubst_id => ids
   end.
 
 Fixpoint unquote_cterm q :=
   match q with
-  | qmeta t => t
+  | qatom t => t
   | qren r t => ren1 (unquote_ren r) (unquote_cterm t)
   | qsubst s t => subst1 (unquote_subst s) (unquote_cterm t)
   end.
 
-(** A substitution/renaming is first a sequence of conses and then a composition
-    of atomic substitutions/renamings.
+(** Evaluation **)
 
-    TODO See where shifts end up.
-*)
-
-Definition normal_ren := (list nat * list (nat → nat))%type.
-
-Definition normal_subst := (list cterm * list (nat → cterm))%type.
-
-Definition normal_cterm := (normal_subst * cterm)%type.
-
-Fixpoint apply_ren (r : list (nat → nat)) n :=
+Fixpoint eval_ren (r : quoted_ren) :=
   match r with
-  | [] => n
-  | ρ :: r => ρ (apply_ren r n)
+  | qren_comp r q =>
+    let r := eval_ren r in
+    let q := eval_ren q in
+    match r, q with
+    | qren_id, _ => q
+    | _, qren_id => r
+    | qren_cons n r, qren_shift => r
+    | _, qren_comp u v => qren_comp (qren_comp r u) v
+    | _, qren_cons n q => qren_cons (unquote_ren r n) (qren_comp r q)
+    | _, _ => qren_comp r q
+    end
+  | qren_cons 0 qren_shift => qren_id
+  | _ => r
   end.
 
-Definition apply_nren (r : normal_ren) (n : nat) :=
-  let (l, r) := r in
-  match nth_error l n with
-  | Some x => x
-  | None => apply_ren r (n - length l)
+Fixpoint eval_subst (s : quoted_subst) : quoted_subst :=
+  match s with
+  | qsubst_comp u v =>
+    let u := eval_subst u in
+    let v := eval_subst v in
+    match u, v with
+    | qsubst_id, _ => v
+    | _, qsubst_id => u
+    | _, qsubst_comp x y => qsubst_comp (qsubst_comp u x) y
+    | _, qsubst_cons t s =>
+      qsubst_cons (subst1 (unquote_subst s) t) (qsubst_comp u s)
+    | _, _ => qsubst_comp u v
+    end
+  | _ => s
   end.
 
-Definition nr_comp (u v : normal_ren) :=
-  let '(ul, ur) := u in
-  let '(vl, vr) := v in
-
-
-Fixpoint nr_comp uc ur vc vr : normal_ren :=
-  match vc with
-  | [] => (uc, ur ++ vr)
-
-Fixpoint nr_comp (u v : normal_ren) :=
-  let (uc, ur) := u in
-  let (vc, vr) := v in
-
-
-Fixpoint ns_comp (u v : normal_subst) :=
-  match u with
-  | [] => v
-  |
-  end.
-
-Fixpoint eval_ren q :=
-  match q with
-  | qren_atom ρ => [ sren ρ ]
-  | qren_comp r q => eval_ren r ++ eval_ren q
-  end.
-
-Fixpoint eval_subst q :=
-  match q with
-  | qsubst_atom σ => [ ssubst σ ]
-  | qsubst_comp s t => eval_subst s ++ eval_subst t
-  end.
-
-Fixpoint eval_cterm q : normal_cterm :=
-  match q with
-  | qmeta t => ([], t)
+Fixpoint eval_cterm (t : quoted_cterm) : quoted_cterm :=
+  match t with
   | qren r t =>
-    let r' := eval_ren r in
-    let '(s',t') := eval_cterm t in
-    (s' ++ r', t')
+    let r := eval_ren r in
+    let t := eval_cterm t in
+    match t with
+    | qren r' t => qren (qren_comp r' r) t
+    | _ => qren r t
+    end
   | qsubst s t =>
-    let s' := eval_subst s in
-    let '(s'',t') := eval_cterm t in
-    (s'' ++ s', t')
-  end.
-
-Inductive normal_subst :=
-| nId
-| nRen (ρ : nat → nat)
-| nSubst (σ : nat → cterm).
-
-Definition ns_comp (s t : normal_subst) :=
-  match s, t with
-  | nId, _ => t
-  | _, nId => s
-  | nRen ρ, nRen ρ' => nRen (funcomp ρ ρ')
-  | nRen ρ, nSubst σ => nSubst (funcomp (ren_cterm ρ) σ)
-  | nSubst σ, nRen ρ => nSubst (funcomp σ ρ)
-  | nSubst σ, nSubst θ => nSubst (funcomp (subst_cterm σ) θ)
-  end.
-
-Definition nscons (t : cterm) s :=
-  match s with
-  | nId => nSubst (scons t cvar)
-  | nRen ρ => nSubst (scons t (funcomp cvar ρ))
-  | nSubst σ => nSubst (scons t σ)
-  end.
-
-(* TODO Instead of all this, have a better normal form than lists
-  of sparts. Like scons should be primitive in it.
-*)
-
-Fixpoint reify_sparts (s : list spart) : normal_subst :=
-  match s with
-  | [] => nId
-  | sren ρ :: s =>
-    match reify_sparts s with
-    | nId => nRen ρ
-    | nRen ρ' => nRen (funcomp ρ ρ')
-    | nSubst σ => nSubst (funcomp (ren_cterm ρ) σ)
+    let s := eval_subst s in
+    let t := eval_cterm t in
+    match t with
+    | qsubst s' t => qsubst (qsubst_comp s' s) t
+    | _ => qsubst s t
     end
-  | ssubst σ :: s =>
-    match reify_sparts s with
-    | nId => nSubst σ
-    | nRen ρ => nSubst (funcomp σ ρ)
-    | nSubst θ => nSubst (funcomp (subst_cterm σ) θ)
-    end
-  | sscons t k :: sshift :: s =>
-    let k' := reify_sparts k in
-    let s' := reify_sparts s in
-    ns_comp k' s'
-  | sscons t k :: s =>
-    let k' := reify_sparts k in
-    let s' := reify_sparts s in
-    ns_comp (nscons t k') s'
-  | sshift :: s =>
-    let s' := reify_sparts s in
-    ns_comp (nRen shift) s'
+  | _ => t
   end.
 
-Fixpoint reify_spart (s : list spart) (t : cterm) : cterm :=
-  match s with
-  | [] => t
-  | sren ρ ::
+(** Correctness **)
 
-Definition reify (t : normal_cterm) : cterm :=
-  let '(s,t) := t in
-  reify_spart s t.
+Lemma eval_ren_sound :
+  ∀ r n,
+    unquote_ren r n = unquote_ren (eval_ren r) n.
+Proof.
+  intros r n.
+  induction r in n |- *.
+  all: try reflexivity.
+  (* TODO Maybe funelim instead *)
+Admitted.
+
+Lemma eval_subst_sound :
+  ∀ s n,
+    unquote_subst s n = unquote_subst (eval_subst s) n.
+Proof.
+Admitted.
+
+Lemma eval_cterm_sound :
+  ∀ t,
+    unquote_cterm t = unquote_cterm (eval_cterm t).
+Proof.
+Admitted.
+
+(** Quoting **)
+
+Ltac quote_ren r :=
+  lazymatch r with
+  | funcomp ?r ?r' =>
+    let q := quote_ren r in
+    let q' := quote_ren r' in
+    constr:(qren_comp q q')
+  | scons ?n ?r =>
+    let q := quote_ren r in
+    constr:(qren_cons n q)
+  | id => constr:(qren_id)
+  | shift => constr:(qren_shift)
+  | S => constr:(qren_shift)
+  | _ => constr:(qren_atom r)
+  end.
+
+Ltac quote_subst s :=
+  lazymatch s with
+  | funcomp (subst_cterm ?s) ?t =>
+    let q := quote_subst s in
+    let q' := quote_subst t in
+    constr:(qsubst_comp q q')
+  | scons ?t ?s =>
+    let q := quote_subst s in
+    constr:(qsubst_cons t q)
+  | ids => constr:(qsubst_id)
+  | _ => constr:(qsubst_atom s)
+  end.
+
+Ltac quote_cterm t :=
+  lazymatch t with
+  | ren1 ?r ?t =>
+    let qr := quote_ren r in
+    let qt := quote_cterm t in
+    constr:(qren qr qt)
+  | subst1 ?s ?t =>
+    let qs := quote_subst s in
+    let qt := quote_cterm t in
+    constr:(qsubst qs qt)
+  | _ => constr:(qatom t)
+  end.
+
+(** Main tactic TODO **)
